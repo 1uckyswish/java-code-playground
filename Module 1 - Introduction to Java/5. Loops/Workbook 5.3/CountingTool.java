7de6d1a2f57a64d83d0a27138a16d493412fe643
Noel Guillen <noelblas274@gmail.com>
2023-11-25 02:49:18 -0500

    started loops section

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class CountingTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Choose a number \n");
+        int answer = scan.nextInt();
 
+        for(int i = 1; i <= answer; i++){
+            System.out.println(i+ "\n");
+        }
         // See Learn the Part for the instructions.
 
         scan.close();
