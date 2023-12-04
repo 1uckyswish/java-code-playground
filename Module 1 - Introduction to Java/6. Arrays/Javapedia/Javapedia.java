754c1c92bfe47ff0cfd86ba4e734dba552f43977
Noel Guillen <noelblas274@gmail.com>
2023-12-03 19:32:25 -0500

    started on tick tack

--- Module 1 - Introduction to Java/6. Arrays/Javapedia/Javapedia.java
+++ Module 1 - Introduction to Java/6. Arrays/Javapedia/Javapedia.java
@@ -7,9 +7,25 @@ public class Javapedia {
         System.out.println("How many historical figures will you register?");
         //Task 1 – Ask the user: how many historical figures will you register?
         //       – Store the value.
+
+        int figuresRegister = scan.nextInt();
         
-        //Task 2 – Create a 2D array with a variable number of rows, and 3 values per row.         
+        //Task 2 – Create a 2D array with a variable number of rows, and 3 values per row.       
+        String[][] database = new String[figuresRegister][3];  
          
+
+         scan.nextLine();
+
+         for(int i = 0; i < database.length; i++){
+            System.out.println("\n\tFigure " + (i+1));
+            System.out.print("\t - Name: ");
+            database[i][0] = scan.nextLine();
+            System.out.print("\t - Date of birth: ");
+             database[i][1] = scan.nextLine();
+            System.out.print("\t - Occupation: ");
+            database[i][2] = scan.nextLine();
+            System.out.print("\n");
+         }
         //Watch out for the nextLine() pitfall. 
         /* Task 3 
         for (____) {
@@ -32,9 +48,18 @@ public class Javapedia {
         */
         System.out.println("These are the values you stored:"); 
         //Task 4: call print2DArray. 
+        print2DArray(database);
 
         System.out.print("\nWho do you want information on? ");  
-        
+        String findMe = scan.nextLine();
+
+        for (int i = 0; i < database.length; i++) {
+            if (database[i][0].equals(findMe)) {
+                System.out.println("\tName: " + database[i][0]);
+                System.out.println("\tDate of birth: " + database[i][1]);
+                System.out.println("\tOccupation: " + database[i][2]);
+            }
+        }
         /*Task 5: Let the user search the database by name. 
             If there's a match:
               print(    tab of space    Name: <name>)
@@ -56,5 +81,18 @@ public class Javapedia {
      *     • each value in database has one space from the other value. 
      *     • print a new line.
      */
+
+    public static void print2DArray(String[][] data){
+      for(int i = 0; i < data.length; i++){
+          System.out.println("\t");
+          for(int j = 0; j < data[i].length; i++){
+            System.out.println(data[i][j] + " ");
+          }
+           System.out.print("\n");
+      }
+
+
+    }
+    
 
 }
---
+++ TDarrays.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class TDarrays {
+
+    public static void main(String[] args) {
+        // int[][] grades = new int[3][4];
+        // grades[0][0] = 72;
+        // grades[0][1] = 72;
+        // grades[0][2] = 72;
+        // grades[0][3] = 72;
+        // grades[1][0] = 100;
+        // System.out.println(grades[1][0]);
+        // System.out.println(Arrays.toString(grades[1]));
+        // System.out.println(Arrays.toString(grades[2]));
+        //  System.out.println(Arrays.toString(grades[1]));
+
+         String[][] table = new String[3][2]; 
+        System.out.println(table[2]);
+        System.out.println(table[2]);
+    }
+
+}
