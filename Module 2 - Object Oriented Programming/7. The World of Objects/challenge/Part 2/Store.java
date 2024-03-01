3f22dfe8cbb75df07967c5e19c075d6ef983b015
Noel Guillen <noelblas274@gmail.com>
2024-02-29 21:00:03 -0500

    final version of movies and movies

--- Module 2 - Object Oriented Programming/7. The World of Objects/challenge/Part 2/Store.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/challenge/Part 2/Store.java
@@ -1,18 +1,20 @@
+import java.util.Arrays;
+
 public class Store {
 
     private Movie[] movies;
 
     public Store() {
-        // TODO
+        this.movies = new Movie[10];
     }
 
     public Movie getMovie(int index) {
-        // TODO 
-        return null;
+        return new Movie(this.movies[index]);
     }
 
     public void setMovie(int index, Movie movie) {
         // TODO 
+         this.movies[index] = new Movie(movie);
     }
 
 
