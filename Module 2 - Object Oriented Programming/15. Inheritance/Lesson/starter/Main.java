200c1085f4e6fead560d3e683e49718e15c4e091
Noel Guillen <noelblas274@gmail.com>
2024-05-28 13:19:18 -0400

    extend classes of product

--- Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
+++ Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
@@ -1,8 +1,16 @@
+import product.Shirt;
+import product.Shirt.Size;
+
 public class Main {
 
     public static void main(String[] args) {
+        Shirt shirt  = new Shirt();
+        shirt.setSize(Size.SMALL);
+        shirt.setBrand("Gucci");
+        shirt.setPrice(3.99);
+        shirt.setColor("Green");
 
-
+        shirt.fold();
 
     }
 
